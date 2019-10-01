5ff3ed929edbcd7d919c3042b339b897deb46b7d
Jam Risser <jam@codejam.ninja>
2019-10-01 01:06:45 -0500

    Improved type bindings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,23 +1,34 @@
-declare class MultithreadConfig<TConfig = Config> {
-  isMaster: () => boolean;
-
-  setConfig<Config = TConfig>(config: Config): Promise<Config>;
-
-  getConfig<Config = TConfig>(): Promise<Config>;
-
-  start(): Promise<void>;
-
-  finish(): Promise<void>;
-
-  preProcess<Config = TConfig>(config: Config): Config | Promise<Config>;
-
-  postProcess<Config = TConfig>(config: Config): Config | Promise<Config>;
-}
-
-declare interface Config {
-  [key: string]: any;
-}
-
 declare module 'multithread-config' {
+  namespace MultithreadConfig {
+    export interface Config {
+      [key: string]: any;
+    }
+
+    export interface Options {
+      socket?: boolean;
+      sync?: boolean;
+      timeout?: number;
+      name?: string;
+    }
+  }
+
+  class MultithreadConfig<TConfig = MultithreadConfig.Config> {
+    constructor(options?: MultithreadConfig.Options);
+
+    isMaster: () => boolean;
+
+    setConfig<Config = TConfig>(config: Config): Promise<Config>;
+
+    getConfig<Config = TConfig>(): Promise<Config>;
+
+    start(): Promise<void>;
+
+    finish(): Promise<void>;
+
+    preProcess<Config = TConfig>(config: Config): Config | Promise<Config>;
+
+    postProcess<Config = TConfig>(config: Config): Config | Promise<Config>;
+  }
+
   export = MultithreadConfig;
 }
